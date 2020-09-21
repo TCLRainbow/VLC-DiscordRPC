5c449dfcfc2b5b27f8910c53b65b4d35e255084c
Pythonic-Rainbow <wongchingho0@gmail.com>
2020-09-21 17:54:37 +0100

    v 0.1

--- cdmusic.py
+++ cdmusic.py
@@ -1,25 +1,41 @@
-import glob
+import asyncio
+import ctypes
 import os
 import time
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
 
 import vlc
 from pypresence import Presence
 
-song_name = input('Enter keyword for the song\n')
-song_file = next(glob.iglob(f'{os.getenv("userprofile")}\\Music\\*{glob.escape(song_name)}*'))
-mplayer = vlc.MediaPlayer(song_file, '-L')
+Tk().withdraw()
+file = askopenfilename(initialdir=os.getenv("userprofile") + '\\Music')
+mplayer = vlc.MediaPlayer(file)
 print('Updating presence')
 RPC = Presence('757258842328399944')
 RPC.connect()
-print(f"Now playing: {song_file}")
-
-
-while True:
-    mplayer.play()
-    time.sleep(0.5)
-    length = mplayer.get_length() / 1000
-    print(length)
-    snap = time.time()
-    RPC.update(details=song_file.split('\\')[-1], start=int(snap), end=int(snap + length))
-    time.sleep(length)
-    mplayer.stop()
+print(f"Now playing: {file}")
+mplayer.play()
+file_name = file.split('/')[-1]
+extension = file_name.split('.')[-1]
+time.sleep(0.5)
+length = mplayer.get_length() / 1000
+media = mplayer.get_media()
+# mediaTrack_pp = ctypes.POINTER(vlc.MediaTrack)()
+# n = vlc.libvlc_media_tracks_get(media, ctypes.byref(mediaTrack_pp))
+# print(n)
+# print(asyncio.get_event_loop().create_task(media.tracks_get()))
+if length > 0:
+    while True:
+        snap = time.time()
+        RPC.update(state=file_name, large_image=extension, start=int(snap), end=int(snap + length))
+        time.sleep(length)
+        mplayer.stop()
+        mplayer.play()
+else:
+    while True:
+        RPC.update(state=file_name, large_image=extension, start=int(time.time()))
+        while mplayer.is_playing():
+            time.sleep(0.5)
+        mplayer.stop()
+        mplayer.play()
